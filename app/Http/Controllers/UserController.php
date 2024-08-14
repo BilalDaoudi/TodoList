2b24c738726e718bac4a1c1449b8923f77eb0e51
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-08-14 10:22:53 +0100

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,23 +9,28 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    
     public function showLoginForm()
     {
         return view('login');
     }
+    
     public function login(Request $request)
     {
         $credentials = $request->only('username', 'password');
         $remember = $request->has('remember_me'); // Vérifie si la case à cocher "Se souvenir de moi" est cochée
         if (Auth::attempt($credentials, $remember)) { // Utilise le paramètre $remember pour se souvenir de l'utilisateur
+                                                     
             if ($remember) {
                 // Créer un cookie de rappel pour se souvenir de l'utilisateur
                 $request->session()->regenerate();
                 $rememberToken = Auth::user()->getRememberToken();
                 cookie()->queue('remember_token', $rememberToken, 60 * 24 * 30); // Le cookie expirera après 30 jours (modifiable selon vos besoins)
             }
+                                                     
             return redirect()->route('todolist');
         }
+        
         return back()->with('erreur', 'Nom d\'utilisateur ou mot de passe incorrect !');
         // $credentials = $request->only('username', 'password');
         // if (Auth::attempt($credentials)) {
@@ -33,6 +38,7 @@ class UserController extends Controller
         // }
         // return back()->with('erreur', 'username or password incorrect !');
     }
+    
     public function logout(Request $request)
     {
         Auth::logout();
@@ -40,10 +46,12 @@ class UserController extends Controller
         $request->session()->regenerateToken();
         return redirect()->route('login');
     }
+    
     public function showInscriptionForm()
     {
         return view('inscription');
     }
+    
     public function register(Request $request)
     {
         $request->validate([
@@ -64,6 +72,7 @@ class UserController extends Controller
             'username.unique' => 'Ce nom d\'utilisateur est déjà utilisé.',
             'email.unique' => 'Cette adresse email est déjà utilisée.'
         ]);
+        
         User::create([
             'username' => $request->username,
             'nom' => $request->nom,
@@ -71,6 +80,7 @@ class UserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+        
         // You can customize the redirect route after registration
         return redirect('/login')->with('success', 'Inscription réussie !');
     }

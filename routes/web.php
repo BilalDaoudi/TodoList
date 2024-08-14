219099ad2096ad3756c28e48f36d2f9d0fbe380c
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-08-14 10:32:26 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,20 @@ Route::get('/', function () {
     return view('login');
 });
 
-Route::get('/login', [UserController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [UserController::class, 'login']);
+// Auth routes
+Route::get('/login', [UserController::class, 'showLoginForm'])->name('login.form');
+Route::post('/login', [UserController::class, 'login'])->name('login');
 Route::get('/logout', [UserController::class, 'logout'])->name('logout');
 
-Route::get('/register', [UserController::class, 'showInscriptionForm']);
-Route::post('/register', [UserController::class, 'register']);
+// Registration routes
+Route::get('/register', [UserController::class, 'showInscriptionForm'])->name('register.form');
+Route::post('/register', [UserController::class, 'register'])->name('register');
 
-Route::get('/todolist/{date?}', [TodoListController::class, 'index'])->name('todolist');
-Route::post('/todolist', [TodoListController::class, 'store'])->name('add');
-Route::delete('/todolist{id}', [TodoListController::class, 'delete'])->name('delete');
-Route::put('/todolist/valider/{id}', [TodoListController::class, 'valider'])->name('valider');
-Route::put('/todolist/play/{id}', [TodoListController::class, 'play'])->name('play');
+// Todo list routes
+Route::prefix('todolist')->group(function () {
+    Route::get('{date?}', [TodoListController::class, 'index'])->name('todolist');
+    Route::post('/', [TodoListController::class, 'store'])->name('todolist.store');
+    Route::delete('{id}', [TodoListController::class, 'delete'])->name('todolist.delete');
+    Route::put('valider/{id}', [TodoListController::class, 'valider'])->name('todolist.valider');
+    Route::put('play/{id}', [TodoListController::class, 'play'])->name('todolist.play');
+});

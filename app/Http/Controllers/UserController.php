0cea506b0f5e523c34af9eb3c4a3552236f8388b
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2025-05-13 15:24:03 +0000

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,20 +1,15 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-
 class UserController extends Controller
 {
-    
     public function showLoginForm()
     {
         return view('login');
     }
-    
     public function login(Request $request)
     {
         $credentials = $request->only('username', 'password');

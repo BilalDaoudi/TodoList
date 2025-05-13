4d5e6be784310c96df73aefab7d686e48ad4607c
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2025-05-13 15:22:26 +0000

    Update TodoListController.php

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -1,18 +1,14 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class TodoListController extends Controller
-{
-    
+{    
     public function index($date = NULL)
     {
-        
         if (!auth()->check()) {
             return redirect()->route('login');
         }

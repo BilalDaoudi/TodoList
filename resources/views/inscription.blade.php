9960a8b9b916b94495255e84b84b7768641172e2
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2025-05-14 13:17:12 +0000

    Update inscription.blade.php

--- resources/views/inscription.blade.php
+++ resources/views/inscription.blade.php
@@ -3,17 +3,11 @@
 <html lang="en">
     
 <head>
-
-    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
-
-
-    
-    
 <body class="bg-gray-100">
     <div class="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div class="max-w-md w-full space-y-8">

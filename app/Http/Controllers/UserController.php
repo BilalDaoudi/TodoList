27dee7de87e878e452ac6f5aaf7958e7b940ca0e
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2025-05-14 13:16:22 +0000

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,9 +1,17 @@
 <?php
+
+
+
+
 namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+
+
+
+
 class UserController extends Controller
 {
     public function showLoginForm()

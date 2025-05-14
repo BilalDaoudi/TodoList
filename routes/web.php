4cdd2bf257d63e4e163ba253ff7306223b2012d1
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2025-05-14 13:18:06 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,22 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
-
-
-
 // Auth routes
 Route::get('/login', [UserController::class, 'showLoginForm'])->name('login.form');
 Route::post('/login', [UserController::class, 'login'])->name('login');
 Route::get('/logout', [UserController::class, 'logout'])->name('logout');
-
-
-
 // Registration routes
 Route::get('/register', [UserController::class, 'showInscriptionForm'])->name('register.form');
 Route::post('/register', [UserController::class, 'register'])->name('register');
-
-
-
 // Todo list routes
 Route::prefix('todolist')->group(function () {
     Route::get('{date?}', [TodoListController::class, 'index'])->name('todolist');
